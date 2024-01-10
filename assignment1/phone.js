3f4332300931030afeeb23043b8c13970ffd52b4
ticek1 <ticek1@udayton.edu>
2024-01-10 17:49:39 -0500

    fixed tab toggle

--- assignment1/phone.js
+++ assignment1/phone.js
@@ -5,13 +5,13 @@ $(document).ready(function() { // do this when the document is loaded
     $("#newContact").hide(); // hide the element with ID "otherElement"
 });
 
-$("#button_tb1").click(function() { // when "button_id" is clicked
+$("#tb1").click(function() { // when "button_id" is clicked
 	$("#dialer").show(); // show the element with ID "element"
 	$("#contact_list").hide(); // hide the element with ID "otherElement"
     $("#newContact").hide(); // hide the element with ID "otherElement"
 });
 
-$("tb2").click(function() { // when "button_id" is clicked
+$("#tb2").click(function() { // when "button_id" is clicked
 	$("#dialer").hide(); // show the element with ID "element"
 	$("#contact_list").show(); // hide the element with ID "otherElement"
     $("#newContact").hide(); // hide the element with ID "otherElement"
