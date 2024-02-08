3de9139177665d44c275f3be1df10b877090f683
ticek1 <ticek1@udayton.edu>
2024-02-08 16:58:43 -0500

    test

--- assignment4/blog.js
+++ assignment4/blog.js
@@ -2,6 +2,11 @@
 
 // Accessibility Buttons
 $("#fontUp").click(function() {
-	$('paragraph').css('font-size', 36);
-    $('caption').css('font-size', 30);
+	$('.paragraph').css('font-size', 36);
+    $('.caption').css('font-size', 30);
+});
+
+$("#fontDown").click(function() {
+	$('#paragraph').css('font-size', 12);
+    $('#caption').css('font-size', 8);
 });
