1c957ff408975be3c1efb184b2aa8a0474f66c68
ticek1 <ticek1@udayton.edu>
2024-02-08 17:42:20 -0500

    font size

--- assignment4/blog.js
+++ assignment4/blog.js
@@ -2,19 +2,19 @@
 
 // Accessibility Buttons
 $("#fontUp").click(function() {
-    let content = document.getElementById("paragraph").style;
-    content.fontSize = content.fontSize + 10;
-    console.log(document.getElementById("paragraph").style.fontSize);
-	// $('.paragraph').css('font-size', '36');
-    // $('.caption').css('font-size', '30');
+    // let content = document.getElementById("paragraph").style;
+    // content.fontSize = content.fontSize + 10;
+    // console.log(document.getElementById("paragraph").style.fontSize);
+	$('.paragraph').css('font-size', '36');
+    $('.caption').css('font-size', '30');
 });
 
 $("#fontDown").click(function() {
-    document.getElementById("paragraph").style.fontSize += 10;
-    console.log(document.getElementById("paragraph").style.fontSize);
+    // document.getElementById("paragraph").style.fontSize += 10;
+    // console.log(document.getElementById("paragraph").style.fontSize);
     // content.fontSize = content.fontSize++;
-	// $('#paragraph').css('font-size', '12');
-    // $('#caption').css('font-size', '8');
+	$('#paragraph').css('font-size', '12');
+    $('#caption').css('font-size', '8');
 });
 
 function fontUp(){
