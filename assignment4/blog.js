c8a36c56ad9f1feba4d40fd8152f4f96bfab6ce6
ticek1 <ticek1@udayton.edu>
2024-02-08 18:31:43 -0500

    formatting

--- assignment4/blog.js
+++ assignment4/blog.js
@@ -2,11 +2,6 @@
 
 // Accessibility Buttons
 $("#fontUp").click(function() {
-    // document.getElementById("paragraph").style.fontSize += 10;
-    // content.fontSize = content.fontSize + 10;
-    // console.log(document.getElementById("paragraph").style.fontSize);
-	// $('.paragraph').css('font-size', 36);
-    // $('.caption').css('font-size', 30);
     pSize = parseInt($('.paragraph').css('font-size'));
     pSize = pSize + 6;
 	$('.paragraph').css('font-size', pSize);
@@ -16,23 +11,6 @@ $("#fontUp").click(function() {
 });
 
 $("#fontDown").click(function() {
-    // let size = document.getElementById("paragraph").style.fontSize;
-    // console.log(document.getElementById("paragraph").style.fontSize);
-    // content.fontSize = content.fontSize++;
-    // let pClass = document.getElementsByClassName('paragraph').style.fontSize + 10;
-    // let cClass = document.getElementsByClassName('caption').style.fontSize - 10;
-    // pSize = 18;
-    // cSize = 15;
-    // for (var e in pClass){
-    //     e.style.fontSize += 1
-    // }
-    // for (var e in cClass){
-    //     if (e == fontSize){
-    //         cSize = e;
-    //     }
-    // }
-    // cSize++;
-    // pSize++;
     pSize = parseInt($('.paragraph').css('font-size'));
     pSize = pSize - 6;
 	$('.paragraph').css('font-size', pSize);
