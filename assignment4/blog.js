05ea184c5267c59219b4f2385529e49eb92789fc
ticek1 <ticek1@udayton.edu>
2024-02-08 17:31:06 -0500

    console logs

--- assignment4/blog.js
+++ assignment4/blog.js
@@ -4,12 +4,14 @@
 $("#fontUp").click(function() {
     let content = document.getElementById("paragraph").style;
     content.fontSize = content.fontSize + 10;
+    console.log(document.getElementById("paragraph").style.fontSize);
 	// $('.paragraph').css('font-size', '36');
     // $('.caption').css('font-size', '30');
 });
 
 $("#fontDown").click(function() {
     document.getElementById("paragraph").style.fontSize += 10;
+    console.log(document.getElementById("paragraph").style.fontSize);
     // content.fontSize = content.fontSize++;
 	// $('#paragraph').css('font-size', '12');
     // $('#caption').css('font-size', '8');
