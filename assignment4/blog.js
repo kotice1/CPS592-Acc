bbb010096ac5dac642729f595ad3cf8729d2aa1c
ticek1 <ticek1@udayton.edu>
2024-02-08 17:25:04 -0500

    test

--- assignment4/blog.js
+++ assignment4/blog.js
@@ -4,11 +4,11 @@
 $("#fontUp").click(function() {
     // let content = document.getElementById("content").style;
     // content.fontSize = content.fontSize++;
-	$('.paragraph').css('font-size', 36);
-    $('.caption').css('font-size', 30);
+	$('.paragraph').css('font-size', '36');
+    $('.caption').css('font-size', '30');
 });
 
 $("#fontDown").click(function() {
-	$('#paragraph').css('font-size', 12);
-    $('#caption').css('font-size', 8);
+	$('#paragraph').css('font-size', '12');
+    $('#caption').css('font-size', '8');
 });
