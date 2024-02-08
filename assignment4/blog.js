684bc0d96525d77937e0a7b42e2064ebf12494e3
ticek1 <ticek1@udayton.edu>
2024-02-08 17:29:17 -0500

    fontsize test

--- assignment4/blog.js
+++ assignment4/blog.js
@@ -2,13 +2,15 @@
 
 // Accessibility Buttons
 $("#fontUp").click(function() {
-    // let content = document.getElementById("content").style;
-    // content.fontSize = content.fontSize++;
-	$('.paragraph').css('font-size', '36');
-    $('.caption').css('font-size', '30');
+    let content = document.getElementById("paragraph").style;
+    content.fontSize = content.fontSize + 10;
+	// $('.paragraph').css('font-size', '36');
+    // $('.caption').css('font-size', '30');
 });
 
 $("#fontDown").click(function() {
-	$('#paragraph').css('font-size', '12');
-    $('#caption').css('font-size', '8');
+    document.getElementById("paragraph").style.fontSize += 10;
+    // content.fontSize = content.fontSize++;
+	// $('#paragraph').css('font-size', '12');
+    // $('#caption').css('font-size', '8');
 });
